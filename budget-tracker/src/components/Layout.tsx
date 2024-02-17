a674db54818b674015fafef75dcf136fc14c174c
Diego Rosales <dalrosales@outlook.com>
2024-02-17 14:15:44 -0500

    Adjusted files to accomdate directory changes

--- budget-tracker/src/components/Layout.tsx
+++ budget-tracker/src/components/Layout.tsx
@@ -1,13 +1,15 @@
-// /src/components/layout.tsx
-
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
-import '../styles/globals.css'; // Assuming your global styles are located in the styles directory
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const Layout: React.FC = ({ children }) => {
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Head>
@@ -15,6 +17,7 @@ const Layout: React.FC = ({ children }) => {
         <meta name="description" content="Your budget tracking application description goes here" />
         {/* Add any other meta tags or links to external stylesheets or scripts */}
       </Head>
+      
       <body className={inter.className}>
         {/* Common layout structure */}
         {children}
