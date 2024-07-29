aa48def8780105ee2b64de24dd67d3ceb9691e08
dalrosales <dalrosales@outlook.com>
2024-07-28 23:25:36 -0400

    Updating nav

--- BudgetTracker/wwwroot/js/site.js
+++ BudgetTracker/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/* SideNav */
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
