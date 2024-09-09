8248143603ad160e088ccd96264f8fad10576e9a
Diego Rosales <dalrosales@outlook.com>
2024-09-09 10:58:54 -0400

    Restarting WalletWatch web project

---
+++ WalletWatch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WalletWatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
