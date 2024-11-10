ad859f966dce154618f2f0cfe6756fa3d5f366e9
Diego Rosales <dalrosales@outlook.com>
2024-11-10 13:53:55 -0500

    Removed WebPack and adjusted project to import only necessary css/js files. Will bundle later.

--- BudgetTracker/wwwroot/js/site.js
+++ BudgetTracker/wwwroot/js/site.js
@@ -1,8 +1,2 @@
-﻿import 'jquery';
-import 'bootstrap';
-import '@popperjs/core';
-import 'jquery-validation';
-import 'jquery-validation-unobtrusive';
-
-// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
