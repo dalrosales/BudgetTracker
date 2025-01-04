da96195c2d7ccb25449758b0db459fdc55945c2e
Diego Rosales <dalrosales@outlook.com>
2025-01-04 01:42:05 -0500

    Added application shell. Beginning mockup for Budgets tab.

---
+++ BudgetTracker/Pages/Budgets.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgetTracker.Pages
+{
+    public class BudgetsModel : PageModel
+    {
+        private readonly ILogger<BudgetsModel> _logger; 
+
+        public BudgetsModel(ILogger<BudgetsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
