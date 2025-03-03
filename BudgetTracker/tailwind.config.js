15d768a7444656d93bc3135d6973b2daa0a6c1d9
Diego Rosales <dalrosales@outlook.com>
2025-03-03 00:30:07 -0500

    Updated app shell

--- BudgetTracker/tailwind.config.js
+++ BudgetTracker/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         "./**/*.{razor,html,cshtml}",
         "./node_modules/flowbite/**/*.js"
     ],
+    darkMode: 'class',
     theme: {
         screens: {
             sm: '480px',
@@ -13,9 +14,44 @@ module.exports = {
         },
         extend: {
             colors: {
-                brightRed:  '#880808',
-            },
+                primary: { "50": "#ecfdf5", "100": "#d1fae5", "200": "#a7f3d0", "300": "#6ee7b7", "400": "#34d399", "500": "#10b981", "600": "#059669", "700": "#047857", "800": "#065f46", "900": "#064e3b", "950": "#022c22" }            },
         },
+        fontFamily: {
+            'body': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+            'sans': [
+                'Inter',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'system-ui',
+                'Segoe UI',
+                'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ]
+        }
     },
     plugins: [
         require('flowbite/plugin')
